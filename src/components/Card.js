2d849e65f007a21fa6bee9fd98812cd1a1a2fb2b
Uri121 <robinov121@gmail.com>
2020-02-04 23:05:25 +0200

    adding icons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,48 @@
 import React from "react";
+import facebook from "../assets/facebook.svg";
+import aws from "../assets/aws.svg";
+import twitter from "../assets/twitter.svg";
+import apple from "../assets/apple.svg";
+import google from "../assets/google.svg";
 
-const Card = ({ title, body }) => {
+const Card = ({ title, body, show }) => {
+  const getIcon = iconName => {
+    let icon;
+    console.log(iconName);
+    if (iconName === "Facebook") {
+      icon = facebook;
+    }
+    if (iconName === "Google") {
+      icon = google;
+    }
+    if (iconName === "Apple") {
+      icon = apple;
+    }
+    if (iconName === "Twitter") {
+      icon = twitter;
+    }
+    if (iconName === "Amazon") {
+      icon = aws;
+    }
+
+    return icon;
+  };
+
+  let icon = getIcon(title);
+  console.log(icon);
+  
   return (
     <div className="card">
       <div className="card-title bold">{title}</div>
+      <img src={icon}  alt="" />
       <div className="card-body">
         <div className="row">
           <div className="bold">Symbol:</div>
           <div>{body.symbol}</div>
+        </div>
+        <div className="row">
+          <div className="bold">Price:</div>
+          <div className="bold">{body.price}</div>
         </div>
         <div className="row">
           <div className="bold">Date:</div>
@@ -16,10 +51,6 @@ const Card = ({ title, body }) => {
         <div className="row">
           <div className="bold">Open:</div>
           <div>{body.open}</div>
-        </div>
-        <div className="row">
-          <div className="bold">Price:</div>
-          <div>{body.price}</div>
         </div>
         <div className="row">
           <div className="bold">Low:</div>
